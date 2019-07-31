785579e426820b79d6b376fed1c2cd195b43e5f7
Pei Ling Koh <s10174164@connect.np.edu.sg>
2019-07-31 16:40:59 +0800

    Update on Background

--- app/src/main/java/com/mad/hera/ChatAdapter.java
+++ app/src/main/java/com/mad/hera/ChatAdapter.java
@@ -38,12 +38,12 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatViewHolders>{
         holder.mMessage.setText(chatList.get(position).getMessage());
         if(chatList.get(position).getCurrentUser()){
             holder.mMessage.setGravity(Gravity.END);
-            holder.mMessage.setTextColor(Color.parseColor("#404040"));
+            holder.mMessage.setTextColor(Color.parseColor("#FFFFFF"));
             holder.mContainer.setBackgroundColor(Color.parseColor("#F4F4F4"));
         }else{
             holder.mMessage.setGravity(Gravity.START);
-            holder.mMessage.setTextColor(Color.parseColor("#FFFFFF"));
-            holder.mContainer.setBackgroundColor(Color.parseColor("#2DB4C8"));
+            holder.mMessage.setTextColor(Color.parseColor("#AEC6CF"));
+            holder.mContainer.setBackgroundColor(Color.parseColor("#F4F4F4"));
         }
 
     }
